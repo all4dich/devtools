411609bcfaf8cb55c9fd2ab2e62d7361f8c4daec
Sunjoo Park <all4dich@gmail.com>
2024-04-29 16:53:24 -0700

    ldap: Remove user from group

--- ldap/remove_user_from_group.sh
+++ ldap/remove_user_from_group.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-set -e
 workdir=$(dirname 0)
 . ${workdir}/set-credentials.sh
 
-# Search Group
 GROUP_DN=`ldapsearch -x -LLL -H ldap://${LDAP_HOST} -D "${BIND_DN}" -w ${BIND_PW} -b "${GROUP_BASE}" -s sub "cn=${1}" | grep dn:|sed 's/dn: //g'`
 echo $GROUP_DN
 # Search User and get dn
@@ -11,20 +9,22 @@ USER_DN=`ldapsearch -x -LLL -H ldap://${LDAP_HOST} -D "${BIND_DN}" -w ${BIND_PW}
 echo $USER_DN
 
 rm -fv *.ldif
-cat << EOF >  update_group1.ldif
+
+cat << EOF >  remove_member.ldif
 dn: $GROUP_DN
 changetype: modify
-add: member
+delete: member
 member: $USER_DN
 EOF
 
-cat << EOF >  update_group2.ldif
-dn: $GROUP_DN
+cat << EOF >  remove_memberUid.ldif
+dn: ${GROUP_DN}
 changetype: modify
-add: memberUid
+delete: memberUid
 memberUid: ${2}
 EOF
 
-ldapmodify -H ${LDAP_SERVER} -D "${BIND_DN}" -w "${BIND_PW}" -f update_group1.ldif
-ldapmodify -H ${LDAP_SERVER} -D "${BIND_DN}" -w "${BIND_PW}" -f update_group2.ldif
-#ldapmodify -H ${LDAP_SERVER} -D "uid=admin,cn=users,dc=nas-admin,dc=nota,dc=ai" -w "wecgyv-dIjrov-tufxo7" -f update_user.ldif
+set -x
+ldapmodify -H ${LDAP_SERVER} -D "${BIND_DN}" -w "${BIND_PW}" -f remove_member.ldif
+ldapmodify -H ${LDAP_SERVER} -D "${BIND_DN}" -w "${BIND_PW}" -f remove_memberUid.ldif
+set +x
