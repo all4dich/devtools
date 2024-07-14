35f13f4411ac1031d0284b12b82f5a03fe891740
Sunjoo Park <all4dich@gmail.com>
2024-07-14 02:59:45 -0700

    GitHub: Final

--- github/backup-github-with-org.py
+++ github/backup-github-with-org.py
@@ -4,6 +4,8 @@ from datetime import datetime, timezone, timedelta
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, String, DateTime, create_engine, update
 from sqlalchemy.ext.declarative import declarative_base
+import git
+from multiprocessing import Pool
 
 Base = declarative_base()
 
@@ -14,6 +16,7 @@ class GithubBackupRepositories(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     repo_id = Column(Integer, unique=True, nullable=False)
     repo_name = Column(String(255), nullable=False)
+    full_name = Column(String(255), nullable=False)
     repo_url = Column(String(255), nullable=False)
     clone_url = Column(String(255), nullable=False)
     ssh_url = Column(String(255))
@@ -27,8 +30,9 @@ TOKEN = os.getenv('TOKEN')
 DAYS = os.getenv('DAYS', 1)
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
+TARGET_DIR = os.getenv('TARGET_DIR')
 now = datetime.now(timezone.utc)
-yesterday = now - timedelta(days=os.getenv('DAYS', 7))
+yesterday = now - timedelta(days=int(DAYS))
 
 engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@10.169.10.12/nota-infra")
 Session = sessionmaker(bind=engine)
@@ -36,6 +40,22 @@ Session = sessionmaker(bind=engine)
 
 def convert_to_datetime(date_string):
     return datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ")
+
+
+def update_local_mirror(each_repo):
+    full_name = each_repo['full_name']
+    ssh_url = each_repo['ssh_url']
+    download_directory = f"{TARGET_DIR}/{full_name}.git"
+    try:
+        if not os.path.exists(download_directory):
+            print(f"Cloning {full_name} to {download_directory}")
+            git.Repo.clone_from(ssh_url, download_directory, mirror=True)
+        else:
+            print(f"Updating {full_name} to {download_directory}")
+            git.Repo(download_directory).remotes.origin.update()
+    except Exception as e:
+        print(f"Error while cloning or pulling {full_name} in {download_directory}: {e}")
+        raise e
 
 
 def get_repos_from_org(org_name):
@@ -57,26 +77,30 @@ def get_repos_from_org(org_name):
             created_at = repo['created_at']
             updated_at = repo['updated_at']
             pushed_at = repo['pushed_at']
+            full_name = repo['full_name']
             pushed_at_object = datetime.strptime(pushed_at, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
             r = session.query(GithubBackupRepositories).filter(GithubBackupRepositories.repo_id == repo_id).all()
             if len(r) == 0:
                 print(f"Repo {repo_name} not found in the database")
                 results.append(
-                    {"repo_id": repo_id, "repo_name": repo_name, "repo_url": repo_url, "clone_url": clone_url,
+                    {"repo_id": repo_id, "repo_name": repo_name, "full_name": full_name, "repo_url": repo_url,
+                     "clone_url": clone_url,
                      "ssh_url": ssh_url, "created_at": created_at, "updated_at": now, "pushed_at": pushed_at})
-                session.add(GithubBackupRepositories(repo_id=repo_id, repo_name=repo_name, repo_url=repo_url,
-                                                     clone_url=clone_url, ssh_url=ssh_url,
+                session.add(GithubBackupRepositories(repo_id=repo_id, repo_name=repo_name, full_name=full_name,
+                                                     repo_url=repo_url, clone_url=clone_url, ssh_url=ssh_url,
                                                      created_at=convert_to_datetime(created_at),
                                                      updated_at=now, pushed_at=pushed_at_object))
             else:
-                print(f"Repo {repo_name} found in the database to be updated")
+                # print(f"Repo {repo_name} found in the database to be updated")
                 if (pushed_at_object > yesterday):
                     print(f"Repo {repo_name} pushed_at is greater than yesterday")
                     session.query(GithubBackupRepositories).filter(GithubBackupRepositories.repo_id == repo_id).update(
-                        {"updated_at": yesterday, "pushed_at": pushed_at})
+                        {"updated_at": yesterday, "pushed_at": pushed_at_object})
                     results.append(
-                        {"repo_id": repo_id, "repo_name": repo_name, "repo_url": repo_url, "clone_url": clone_url,
-                         "ssh_url": ssh_url, "created_at": created_at, "updated_at": yesterday, "pushed_at": pushed_at})
+                        {"repo_id": repo_id, "repo_name": repo_name, "full_name": full_name, "repo_url": repo_url,
+                         "clone_url": clone_url,
+                         "ssh_url": ssh_url, "created_at": created_at, "updated_at": yesterday,
+                         "pushed_at": pushed_at_object})
         page_number += 1
         url = f"{url_main}&page={page_number}"
         response = requests.get(url, headers={"Authorization": "token " + TOKEN})
@@ -87,4 +111,6 @@ def get_repos_from_org(org_name):
 
 
 r = get_repos_from_org(ORG)
-print(r)
+
+with Pool(32) as p:
+    r = p.map(update_local_mirror, r)
