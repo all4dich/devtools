19ab16f0fad23c01437f611b5995dba6e96cfa7a
Sunjoo Park <all4dich@gmail.com>
2024-07-26 11:44:59 -0700

    gitlab: Create a project in the group
    :Issues:
    IM-628

--- gitlab/call-gitlab-api.py
+++ gitlab/call-gitlab-api.py
@@ -18,5 +18,33 @@ def get_project_list():
     print(response.json())
 
 
+def get_namespace_id(org_name: str):
+    url = f"{GITLAB_URL}/api/v4/namespaces"
+    headers = {"Authorization": f"Bearer {GITLAB_TOKEN}", "Content-Type": "application/json"}
+    response = requests.get(url, headers=headers, verify=False)
+    namespace_id = ""
+    for namespace in response.json():
+        if namespace["name"] == org_name:
+            namespace_id = namespace["id"]
+            break
+    return namespace_id
+
+def create_project_repo(org_name: str, repo_name: str):
+    url = f"{GITLAB_URL}/api/v4/projects"
+    headers = {"Authorization": f"Bearer {GITLAB_TOKEN}"}
+    data = {
+        "name": repo_name,
+        "description": "This is a project",
+        "path": repo_name,
+        "namespace_id": get_namespace_id(org_name),
+        "initialize_with_readme": "false",
+        "visibility": "private"
+    }
+    response = requests.post(url, headers=headers, data=data, verify=False)
+    print(response.json())
+    print(response.status_code)
+
+
 if __name__ == "__main__":
-    get_project_list()
+    #get_project_list()
+    create_project_repo("nota-infra", "test-repo2")
