ae51dec2225f7a8173415c32f8c13db1320757b9
Sunjoo Park <all4dich@gmail.com>
2024-08-02 21:04:05 -0700

    gitlab: Update get_pat_list()
    :Issues:
    IM-640

--- gitlab/tokens/pat.py
+++ gitlab/tokens/pat.py
@@ -6,7 +6,19 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 
-def get_pat_list(uid: str = None, per_page: int = 100):
+def get_user_info(user_id: str):
+    logging.debug(f"Get user info for user ID: {user_id}")
+    GITLAB_TOKEN = os.environ["GITLAB_TOKEN"]
+    GITLAB_URL = os.environ["GITLAB_URL"]
+    url = f"{GITLAB_URL}/api/v4/users/{user_id}"
+    response = requests.get(url, headers={"Authorization": f"Bearer {GITLAB_TOKEN}"})
+    user = response.json()
+    logging.debug(
+        f"User ID: {user['id']}, User Name: {user['name']}, Username: {user['username']}, Email: {user['email']}")
+    return user
+
+
+def get_pat_list(uid: str = None, state: str = None, per_page: int = 100):
     GITLAB_TOKEN = os.environ["GITLAB_TOKEN"]
     GITLAB_URL = os.environ["GITLAB_URL"]
     pat_list = []
@@ -16,11 +28,18 @@ def get_pat_list(uid: str = None, per_page: int = 100):
     else:
         # If uid is provided, get PATs for that user
         url = f"{GITLAB_URL}/api/v4/personal_access_tokens?user_id={uid}&per_page={per_page}"
+    if state is not None:
+        # If state is provided, get PATs with that state
+        url = f"{url}&state={state}"
     response = requests.get(url, headers={"Authorization": f"Bearer {GITLAB_TOKEN}"})
     while len(response.json()) > 0:
         # Loop through all PATs and merge them in a list
         for pat in response.json():
-            logging.info(f"ID: {pat['id']}, Name: {pat['name']}, Expires At: {pat['expires_at']}")
+            user_id = pat["user_id"]
+            user_info = get_user_info(user_id)
+            user_name = user_info["name"]
+            active = pat["active"]
+            logging.info(f"User Name: {user_name}, User ID: {user_id}, PAT ID: {pat['id']}, PAT Name: {pat['name']}, Activated: {active}, Expires At: {pat['expires_at']}")
             pat_list.append(pat)
         if "next" not in response.links:
             break
