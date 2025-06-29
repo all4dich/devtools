357b91ddd26e29a770eaa0f02ede4c6ca5807178
Sunjoo Park <all4dich@gmail.com>
2025-06-29 10:55:59 +0900

    refactor(k8s): update instance IP retrieval syntax and improve echo messages

--- k8s_cluster/aws/aws-k8s-master.sh
+++ k8s_cluster/aws/aws-k8s-master.sh
@@ -14,12 +14,12 @@ aws ec2 start-instances --instance-ids ${INSTANCE_ID} --region ${REGION_NAME}
 #echo "Waiting for instance to enter 'running' state..."
 aws ec2 wait instance-running --instance-ids ${INSTANCE_ID} --region ${REGION_NAME}
 
-INSTANCE_IP=`aws ec2 describe-instances \
+INSTANCE_IP=$(aws ec2 describe-instances \
   --instance-ids ${INSTANCE_ID} \
   --region ${REGION_NAME} \
   --query "Reservations[].Instances[].PublicIpAddress" \
-  --output text`
+  --output text)
 
-echo "k8s worker : ${INSTANCE_IP}"
+echo "k8s master : ${INSTANCE_IP}"
 echo "Region: ${REGION_NAME}"
 ssh -i ~/.ssh/all4dich-aws-key.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@${INSTANCE_IP}
--- k8s_cluster/aws/aws-k8s-worker.sh
+++ k8s_cluster/aws/aws-k8s-worker.sh
@@ -14,11 +14,11 @@ aws ec2 start-instances --instance-ids ${INSTANCE_ID} --region ${REGION_NAME}
 #echo "Waiting for instance to enter 'running' state..."
 aws ec2 wait instance-running --instance-ids ${INSTANCE_ID} --region ${REGION_NAME}
 
-INSTANCE_IP=`aws ec2 describe-instances \
+INSTANCE_IP=$(aws ec2 describe-instances \
   --instance-ids ${INSTANCE_ID} \
   --region ${REGION_NAME} \
   --query "Reservations[].Instances[].PublicIpAddress" \
-  --output text`
+  --output text)
 
 echo "k8s worker : ${INSTANCE_IP}"
 echo "Region: ${REGION_NAME}"
